4d0393d6df3f599c652dcfe07ad23ae7c12f8d83
Nikhil <nikhilbisht0026@gmail.com>
2022-09-24 22:07:46 +0530

    oops lab codes updated

---
+++ Demo5_2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+interface A{
+    void meth1(int x);
+    void meth2(int y);
+}
+
+class MyClass implements A{
+    public void meth1(int x){
+        System.out.println("This first method calculate square");
+        System.out.println("The square is: "+x*x);
+        System.out.println();
+    }
+
+    public void meth2(int y){
+        int fact=1;
+        System.out.println("This second method calculates factorial");
+        for(int i=1; i<=y; i++){
+            fact=fact*i;
+        }
+        System.out.println("Factorial of the number is: "+fact);
+    }
+}
+
+public class Demo5_2 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int a=sc.nextInt();
+
+        MyClass c=new MyClass();
+        c.meth1(a);
+        c.meth2(a);
+    }    
+}
