b78502792f921df7185eb93e970346b5bac691bd
Nikhil <nikhilbisht0026@gmail.com>
2022-11-16 10:22:35 +0530

    Convert all String elements to UpperCase

---
+++ Demo7_5.java
@@ -0,0 +1,33 @@
+/*      Write a java program to throw an exception  for an employee details. 
+        If an employee name is a number, a name exception must be thrown.
+        If an employee age is greater than 50, an age exception must be thrown. 
+	    Or else an object must be created for the entered employee details */	
+
+import java.util.*;
+
+class Employee{
+    String name;
+    int age;
+
+    Employee(){
+        this.name= name;
+        this.age=age;
+    }
+
+    public void printDetails(String name, int age){
+        System.out.println("Name of employee is: "+name);
+        System.out.println("Age of the employee is: "+age);
+    }
+}
+
+public class Demo7_5 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter employee name: ");
+        String name= sc.next();
+        System.out.println("Enter employee age: ");
+        int age= sc.nextInt();
+       
+    }
+}
---
+++ Demo8_2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Demo8_2{
+
+   public static void main(String args[])
+   {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter any string to convert to uppercase: ");
+      String strInput = sc.nextLine();
+      char[] ch = strInput.toCharArray();
+      for(int a = 0; a < ch.length; a++)
+      {
+         if(ch[a] >= 'a' && ch[a] <= 'z')
+         {
+            ch[a] = (char)((int)ch[a] - 32);
+         }
+      }
+      System.out.println("string in luppercase is: ");
+      for(int a = 0; a < ch.length; a++)
+      { 
+         System.out.print(ch[a]);
+      }
+      sc.close();
+   }
+}
