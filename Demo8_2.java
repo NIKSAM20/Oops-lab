fa3c709c3bab0d63881d9e51a394aafba977fe39
Nikhil <nikhilbisht0026@gmail.com>
2022-11-29 22:13:23 +0530

    Convert string to uppercase using StringBuffer

--- Demo8_2.java
+++ Demo8_2.java
@@ -1,24 +1,17 @@
-import java.util.Scanner;
-class Demo8_2{
-
-   public static void main(String args[])
-   {
-      Scanner sc = new Scanner(System.in);
-      System.out.println("Enter any string to convert to uppercase: ");
-      String strInput = sc.nextLine();
-      char[] ch = strInput.toCharArray();
-      for(int a = 0; a < ch.length; a++)
-      {
-         if(ch[a] >= 'a' && ch[a] <= 'z')
-         {
-            ch[a] = (char)((int)ch[a] - 32);
+import java.util.*; 
+public class Demo8_2 {
+   public static void main(String[] args) {
+      try (Scanner sc = new Scanner(System.in)) { 
+         StringBuffer str = new StringBuffer(); 
+         System.out.print("Enter a String: "); 
+         str.append(sc.nextLine());
+         for (int i = 0; i < str.length(); i++) { 
+            char ch = str.charAt(i);
+            if (ch >= 97 && ch <= 122) { 
+               str.replace(i,i+1,String.valueOf((char)(ch-32)));
+            }
          }
+         System.out.println(str);
       }
-      System.out.println("string in Uppercase is: ");
-      for(int a = 0; a < ch.length; a++)
-      { 
-         System.out.print(ch[a]);
-      }
-      sc.close();
    }
-}
+}   
