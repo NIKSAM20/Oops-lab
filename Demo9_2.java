0acca05b5f66817aa3da64fd3770f10835917304
Nikhil <nikhilbisht0026@gmail.com>
2022-11-30 09:44:27 +0530

    Printing odd and even numbers using threads

---
+++ Demo9_2.java
@@ -0,0 +1,22 @@
+class Even extends Thread{
+    public void run(){
+        for(int i=2;i<=20;i+=2){
+            System.out.println("Even: "+i);
+        }
+    }
+}
+class Odd extends Thread{
+    public void run(){
+        for(int i=1;i<=20;i+=2){
+            System.out.println("Odd: "+i);
+        }
+    }
+}
+public class Demo9_2 {
+    public static void main(String[] args) {
+        Even e1 = new Even();
+        Odd o1 = new Odd();
+        e1.start();
+        o1.start();
+    }
+}    
