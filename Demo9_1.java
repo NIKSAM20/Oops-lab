0c9383de19c84318ae010d5fb0927aa3efbcb7f6
Nikhil <nikhilbisht0026@gmail.com>
2022-11-30 09:39:53 +0530

    Extend thread class and Runnable Interface

--- Demo9_1.java
+++ Demo9_1.java
@@ -6,7 +6,19 @@ public class Demo9_1 extends Thread{
     }
     public static void main(String[] args) { 
         Demo9_1 q1 = new Demo9_1(); 
-        q1.run();
+        q1.start();
+    }
+}
+public class Question1b implements Runnable { 
+    public void run(){
+        for(int i=0;i<10;i++){ 
+            System.out.println("MultiThreading!");
+        }
+    }
+    public static void main(String[] args) { 
+        Question1b q1b = new Question1b(); 
+        Thread t1 = new Thread(q1b); 
+        t1.start();
     }
 }
     
